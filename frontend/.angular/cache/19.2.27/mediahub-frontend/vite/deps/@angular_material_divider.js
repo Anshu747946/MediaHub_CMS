d365641cac796e05b35c41c487dc2ccc463cf7fc
Apple <apple@example.com>
2026-08-04 16:47:46 +0530

    Initial project commit

---
+++ frontend/.angular/cache/19.2.27/mediahub-frontend/vite/deps/@angular_material_divider.js
@@ -0,0 +1,19 @@
+import {
+  MatDivider,
+  MatDividerModule
+} from "./chunk-2DYLMXTR.js";
+import "./chunk-IBYU652R.js";
+import "./chunk-E6RLKV7P.js";
+import "./chunk-DMGYNBAD.js";
+import "./chunk-4UBEPPRW.js";
+import "./chunk-BIGK7GFW.js";
+import "./chunk-KWB6JEQH.js";
+import "./chunk-43U6UUQ6.js";
+import "./chunk-J6YSA62T.js";
+import "./chunk-XVJVK3DV.js";
+import "./chunk-PV7DW26G.js";
+import "./chunk-IVSRFTZX.js";
+export {
+  MatDivider,
+  MatDividerModule
+};
---
+++ frontend/.angular/cache/19.2.27/mediahub-frontend/vite/deps/@angular_material_select.js
@@ -0,0 +1,171 @@
+import {
+  MAT_SELECT_CONFIG,
+  MAT_SELECT_SCROLL_STRATEGY,
+  MAT_SELECT_SCROLL_STRATEGY_PROVIDER,
+  MAT_SELECT_SCROLL_STRATEGY_PROVIDER_FACTORY,
+  MAT_SELECT_TRIGGER,
+  MatOptgroup,
+  MatOption,
+  MatSelect,
+  MatSelectChange,
+  MatSelectModule,
+  MatSelectTrigger
+} from "./chunk-2KUQ5OI3.js";
+import "./chunk-ZQZ6PSEW.js";
+import "./chunk-4YN5Z6KE.js";
+import "./chunk-RSCQ6S2P.js";
+import "./chunk-IDA22ZWM.js";
+import "./chunk-UF7FRXPC.js";
+import {
+  MatError,
+  MatFormField,
+  MatHint,
+  MatLabel,
+  MatPrefix,
+  MatSuffix
+} from "./chunk-BRJ6H2MG.js";
+import "./chunk-SVYNCHRR.js";
+import "./chunk-KSZNL5TO.js";
+import "./chunk-XWAWHXZR.js";
+import "./chunk-JPNKHW2J.js";
+import "./chunk-JD2IV3DO.js";
+import "./chunk-OSRRUTVM.js";
+import "./chunk-RLLJCJ3P.js";
+import "./chunk-42FJBLFI.js";
+import "./chunk-IBYU652R.js";
+import "./chunk-2O4WY5GE.js";
+import "./chunk-E6RLKV7P.js";
+import "./chunk-DMGYNBAD.js";
+import "./chunk-4UBEPPRW.js";
+import "./chunk-BIGK7GFW.js";
+import "./chunk-KWB6JEQH.js";
+import "./chunk-43U6UUQ6.js";
+import "./chunk-J6YSA62T.js";
+import "./chunk-XVJVK3DV.js";
+import "./chunk-PV7DW26G.js";
+import "./chunk-IVSRFTZX.js";
+
+// node_modules/@angular/material/fesm2022/select.mjs
+var matSelectAnimations = {
+  // Represents
+  // trigger('transformPanelWrap', [
+  //   transition('* => void', query('@transformPanel', [animateChild()], {optional: true})),
+  // ])
+  /**
+   * This animation ensures the select's overlay panel animation (transformPanel) is called when
+   * closing the select.
+   * This is needed due to https://github.com/angular/angular/issues/23302
+   */
+  transformPanelWrap: {
+    type: 7,
+    name: "transformPanelWrap",
+    definitions: [{
+      type: 1,
+      expr: "* => void",
+      animation: {
+        type: 11,
+        selector: "@transformPanel",
+        animation: [{
+          type: 9,
+          options: null
+        }],
+        options: {
+          optional: true
+        }
+      },
+      options: null
+    }],
+    options: {}
+  },
+  // Represents
+  // trigger('transformPanel', [
+  //   state(
+  //     'void',
+  //     style({
+  //       opacity: 0,
+  //       transform: 'scale(1, 0.8)',
+  //     }),
+  //   ),
+  //   transition(
+  //     'void => showing',
+  //     animate(
+  //       '120ms cubic-bezier(0, 0, 0.2, 1)',
+  //       style({
+  //         opacity: 1,
+  //         transform: 'scale(1, 1)',
+  //       }),
+  //     ),
+  //   ),
+  //   transition('* => void', animate('100ms linear', style({opacity: 0}))),
+  // ])
+  /** This animation transforms the select's overlay panel on and off the page. */
+  transformPanel: {
+    type: 7,
+    name: "transformPanel",
+    definitions: [{
+      type: 0,
+      name: "void",
+      styles: {
+        type: 6,
+        styles: {
+          opacity: 0,
+          transform: "scale(1, 0.8)"
+        },
+        offset: null
+      }
+    }, {
+      type: 1,
+      expr: "void => showing",
+      animation: {
+        type: 4,
+        styles: {
+          type: 6,
+          styles: {
+            opacity: 1,
+            transform: "scale(1, 1)"
+          },
+          offset: null
+        },
+        timings: "120ms cubic-bezier(0, 0, 0.2, 1)"
+      },
+      options: null
+    }, {
+      type: 1,
+      expr: "* => void",
+      animation: {
+        type: 4,
+        styles: {
+          type: 6,
+          styles: {
+            opacity: 0
+          },
+          offset: null
+        },
+        timings: "100ms linear"
+      },
+      options: null
+    }],
+    options: {}
+  }
+};
+export {
+  MAT_SELECT_CONFIG,
+  MAT_SELECT_SCROLL_STRATEGY,
+  MAT_SELECT_SCROLL_STRATEGY_PROVIDER,
+  MAT_SELECT_SCROLL_STRATEGY_PROVIDER_FACTORY,
+  MAT_SELECT_TRIGGER,
+  MatError,
+  MatFormField,
+  MatHint,
+  MatLabel,
+  MatOptgroup,
+  MatOption,
+  MatPrefix,
+  MatSelect,
+  MatSelectChange,
+  MatSelectModule,
+  MatSelectTrigger,
+  MatSuffix,
+  matSelectAnimations
+};
+//# sourceMappingURL=@angular_material_select.js.map
